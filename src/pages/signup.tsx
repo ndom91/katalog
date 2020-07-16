26f101ce07548c21414fbb51821334e4dffbb7f8
ndo@$(hostname) <yo@ndo.dev>
2020-07-16 16:06:12 +0200

    fix: build

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Layout from '../components/Layout'
 import Router from 'next/router'
-import { withApollo } from '../apollo/client'
+import { withApollo } from '../../apollo/client'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks'
 
@@ -36,23 +36,24 @@ function Signup(props) {
               },
             })
             Router.push('/')
-          }}>
+          }}
+        >
           <h1>Signup user</h1>
           <input
             autoFocus
             onChange={e => setName(e.target.value)}
-            placeholder="Name"
-            type="text"
+            placeholder='Name'
+            type='text'
             value={name}
           />
           <input
             onChange={e => setEmail(e.target.value)}
-            placeholder="Email address)"
-            type="text"
+            placeholder='Email address)'
+            type='text'
             value={email}
           />
-          <input disabled={!name || !email} type="submit" value="Signup" />
-          <a className="back" href="#" onClick={() => Router.push('/')}>
+          <input disabled={!name || !email} type='submit' value='Signup' />
+          <a className='back' href='#' onClick={() => Router.push('/')}>
             or Cancel
           </a>
         </form>
