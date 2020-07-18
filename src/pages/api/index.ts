97cc7524795a3f147a52e2141a7d73798ef7410c
ndo@$(hostname) <yo@ndo.dev>
2020-07-18 17:38:46 +0200

    update: build cmd

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,6 +1,6 @@
-import app, { server } from "nexus";
-import "../../graphql/schema"; // we'll create this file in a second!
+import app, { server } from 'nexus'
+import '../../graphql/schema'
 
-app.assemble();
+app.assemble()
 
 export default server.handlers.graphql
