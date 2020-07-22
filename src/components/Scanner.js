20d0f0110151bc7b7824b0e2365a7e267f0d9b21
ndo@$(hostname) <yo@ndo.dev>
2020-07-22 23:07:30 +0200

    update: Scanner test 2

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 // import QrReader from 'react-qr-reader'
-import { message } from 'antd'
+import { message, Select } from 'antd'
+
+const { Option } = Select
 
 const QrReader = dynamic(() => import('react-qr-scanner'), {
   ssr: false,
@@ -10,6 +12,36 @@ const QrReader = dynamic(() => import('react-qr-scanner'), {
 const Scanner = () => {
   const [errorMsg, setErrorMsg] = useState('')
   const [result, setResult] = useState('')
+  const [loading, setLoading] = useState('')
+  const [cameraId, setCameraId] = useState('')
+  const [devices, setDevices] = useState('')
+  useEffect(() => {
+    if (navigator) {
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then(cameras => {
+          const videoSelect = []
+          cameras.forEach(device => {
+            if (device.kind === 'videoinput') {
+              videoSelect.push(device)
+            }
+          })
+          return videoSelect
+        })
+        .then(cameras => {
+          setDevices(cameras)
+          setLoading(false)
+          setCameraId(cameras[0].deviceId)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }, [])
+
+  const selectCamera = () => {
+    return cameraId
+  }
   const QrSuccess = data => {
     console.log(data)
     message.success('Success')
@@ -29,12 +61,25 @@ const Scanner = () => {
 
   return (
     <>
+      <Select
+        onChange={value => {
+          setCameraId(value)
+        }}
+      >
+        {devices &&
+          devices.map(device => (
+            <Option key={device.deviceId} value={device.deviceId}>
+              {deviceInfo.label || `camera ${index}`}
+            </Option>
+          ))}
+      </Select>
       <QrReader
         delay={100}
+        gg
         style={{ height: 240, width: 320 }}
         onError={QrError}
         onScan={QrSuccess}
-        chooseDeviceId={chooseDevice}
+        chooseDeviceId={selectCamera}
       />
       {result}
     </>
