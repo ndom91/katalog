96a82c81bba65a7c8751ccbd0e5dfaeb8c565b5e
ndom91 <yo@ndo.dev>
2020-11-01 16:08:11 +0100

    fix: disable sentry rewriteframes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,23 +3,23 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Provider } from 'next-auth/client'
 import * as Sentry from '@sentry/react'
-import getConfig from 'next/config'
-import { RewriteFrames } from '@sentry/integrations'
+// import getConfig from 'next/config'
+// import { RewriteFrames } from '@sentry/integrations'
 import { Integrations } from '@sentry/tracing'
 import '../style/katalog.less'
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
-  const config = getConfig()
-  const distDir = `${config.serverRuntimeConfig.rootDir}/.next`
+  // const config = getConfig()
+  // const distDir = `${config.serverRuntimeConfig.rootDir}/.next`
   Sentry.init({
     enabled: process.env.NODE_ENV === 'production',
     integrations: [
-      new RewriteFrames({
-        iteratee: frame => {
-          frame.filename = frame.filename.replace(distDir, 'app:///_next')
-          return frame
-        },
-      }),
+      // new RewriteFrames({
+      //   iteratee: frame => {
+      //     frame.filename = frame.filename.replace(distDir, 'app:///_next')
+      //     return frame
+      //   },
+      // }),
       new Integrations.BrowserTracing(),
     ],
     tracesSampleRate: 1.0,
